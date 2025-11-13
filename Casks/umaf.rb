4b42761d22f47550e6f7e7153d6153846e35af50
jpsweeney97 <jpsweeney97@gmail.com>
2025-11-13 00:12:47 -0500

    Add UMAF cask for v0.1.0

---
+++ Casks/umaf.rb
@@ -0,0 +1,9 @@
+cask "umaf" do
+  version "0.1.0"
+  sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
+  url "https://github.com/jpsweeney97/umaf-app/releases/download/v0.1.0/UMAF.dmg"
+  name "UMAF"
+  desc "Unified Media/Metadata Application Framework"
+  homepage "https://github.com/jpsweeney97/umaf-app"
+  app "UMAF.app"
+end
