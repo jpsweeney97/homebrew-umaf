a49c52138b5e53cd8b1f5db6be29d802002c590d
jpsweeney97 <jpsweeney97@gmail.com>
2025-11-13 01:28:59 -0500

    chore: update UMAF cask sha256 for v0.1.6

--- Casks/umaf.rb
+++ Casks/umaf.rb
@@ -1,6 +1,6 @@
 cask "umaf" do
   version "0.1.6"
-  sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
+  sha256 "f5244788b1665fc2147760377ac59a03703b3c389e0a372ee0236271bc6d9ca9"
   url "https://github.com/jpsweeney97/umaf-app/releases/download/v0.1.6/UMAF.dmg"
   name "UMAF"
   desc "Unified Media/Metadata Application Framework"
